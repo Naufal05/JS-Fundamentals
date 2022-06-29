03ffafec5676a583a307a9b1630369b1386b657b
MOHAMED NAUFAL <mnaufalmz@gmail.com>
2022-06-29 06:02:19 +0530

    operators basics

---
+++ operators.js
@@ -0,0 +1,31 @@
+var a = 5,
+  b = 10;
+
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var mul = a * b;
+console.log(mul);
+
+var div = a / b;
+console.log(div);
+
+var inc = ++a;
+console.log(inc);
+
+var dec = --b;
+console.log(dec);
+
+var test = "Learning Javascript with Bottleneck";
+
+console.log(test.length); //length
+console.log(test.slice(25, 35)); //slice
+console.log(test.replace("Javascript", "Web3.0")); //replace
+console.log(test.toUpperCase()); //uppercare
+
+console.log(test.concat(" Web3.0 is future")); //concat
+
+console.log("one" < 8);
